d7b1109ee229ec2da602ab5699398c25fe8db8ea
Brian Naqica <bnaqica@yahoo.com>
2019-12-08 19:51:33 -0700

    Initial skeleton commit

---
+++ src/test/java/com/bnaqica/schoolgrades/SchoolGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnaqica.schoolgrades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolGradesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
